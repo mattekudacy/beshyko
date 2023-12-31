370791a70d660010687820ab2ed6f4b63ab10147
mattekudacy <cyrus2952@gmail.com>
2023-12-31 16:08:32 +0800

    added copy-paste workaround

--- beshy.py
+++ beshy.py
@@ -1,20 +1,14 @@
-# import streamlit as st
-
-# st.title('🤸🏻Malungkot ang beshy ko generator🤸🏻')
-
-# text = st.text_input('Enter text here').upper()
-# join = '🤸🏻'.join(text.split(' '))
-# st.text_area('Generated Text', value=join, height=200, key='generated_text')
-# # create a button that will copy the text to clipboard
-# if st.button('Copy to clipboard'):
-#     st.write('Copied to clipboard!')
-#     st.experimental_set_query_params(text=join)
 import streamlit as st
+
 def main():
-    st.title("Copy Paste in streamlit")
-    pathinput = st.text_input("Enter your Path:")
-    #you can place your path instead
-    Path = f'''{pathinput}'''
-    st.code(Path, language="python")
-    st.markdown("Now you get option to copy")
-if __name__ == "__main__":main()
+    st.title('🤸🏻MALUNGKOT🤸🏻ANG🤸🏻BESHY🤸🏻KO🤸🏻GENERATOR🤸🏻')
+    text = st.text_input('Enter text here').upper()
+    join = '🤸🏻'.join(text.split(' '))
+
+    Path_with_newlines = '\n'.join([join[i:i+70] for i in range(0, len(join), 50)])
+    print(Path_with_newlines)
+    st.code('''{}'''.format(Path_with_newlines), language="python")
+    st.markdown("Copy your text!")
+
+if __name__ == "__main__":
+    main()
