76cc81f59a566cba0481f5b1b18c88995d7127cb
Cyrus Mante <74973194+mattekudacy@users.noreply.github.com>
2024-03-30 09:40:59 +0800

    changed UI

--- beshy.py
+++ beshy.py
@@ -3,12 +3,13 @@ import streamlit as st
 def main():
     st.title('🤸🏻MALUNGKOT🤸🏻ANG🤸🏻BESHY🤸🏻KO🤸🏻GENERATOR🤸🏻')
     text = st.text_input('Enter text here').upper()
-    join = '🤸🏻'.join(text.split(' '))
-
-    Path_with_newlines = '\n'.join([join[i:i+70] for i in range(0, len(join), 50)])
-    print(Path_with_newlines)
-    st.code('''{}'''.format(Path_with_newlines), language="python")
-    st.markdown("Copy your text!")
+    while text is not None:
+        join = '🤸🏻'.join(text.split(' '))
+    
+        Path_with_newlines = '\n'.join([join[i:i+70] for i in range(0, len(join), 50)])
+        print(Path_with_newlines)
+        st.markdown("Copy your text!")
+        st.code('''{}'''.format(Path_with_newlines), language="python")
 
 if __name__ == "__main__":
     main()
