2267f39200eb80db2e67bd2ab1550829bc7092de
Cyrus Mante <74973194+mattekudacy@users.noreply.github.com>
2024-03-30 09:47:42 +0800

    changed UI

--- beshy.py
+++ beshy.py
@@ -3,11 +3,10 @@ import streamlit as st
 def main():
     st.title('🤸🏻MALUNGKOT🤸🏻ANG🤸🏻BESHY🤸🏻KO🤸🏻GENERATOR🤸🏻')
     text = st.text_input('Enter text here').upper()
-    while text is not None:
+    if text:
         join = '🤸🏻'.join(text.split(' '))
     
         Path_with_newlines = '\n'.join([join[i:i+70] for i in range(0, len(join), 50)])
-        print(Path_with_newlines)
         st.markdown("Copy your text!")
         st.code('''{}'''.format(Path_with_newlines), language="python")
 
